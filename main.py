83360c775d41ad429440c11e1faa2b340bcf9127
spuun <kek@spuun.art>
2022-11-21 05:56:31 +0700

    Add a little note

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from gooey import Gooey, GooeyParser
 
 @Gooey(advanced=True, program_name="SEBroken", tabbed_groups=True, image_dir="./icons")
 def main():
-    parser = GooeyParser(prog="SEBroken", description="An SEB file patcher")
+    parser = GooeyParser(prog="SEBroken", description="An SEB file patcher.")
     required_args = parser.add_argument_group("Required Arguments")
     opt_args = parser.add_argument_group("Optional Arguments")
     required_args.add_argument(
         "-f",
         "--filename",
-        help="The SEB file to process.",
+        help="The SEB file to process\n(NOTE: OUTPUTS ARE IN THE SAME DIRECTORY AS THIS).",
         default="encrypted.seb",
         widget="FileChooser",
         required=True,
